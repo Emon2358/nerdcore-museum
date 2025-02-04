c8c2f266e5f736842de7d4bee0137ae8b8511fab
ktjcgd <144606308+Emon2358@users.noreply.github.com>
2025-02-04 09:28:23 +0900

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # ダウンロードフォルダ
-DOWNLOAD_DIR = "downloads2"
+DOWNLOAD_DIR = "downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 class LinkParser(HTMLParser):
